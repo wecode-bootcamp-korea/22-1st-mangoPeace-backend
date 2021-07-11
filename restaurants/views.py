2b253f3aee20488a76226377493cfdff61c9125d
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 00:11:27 +0900

    ADD : restaurant detail image view 기능 구현

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http        import JsonResponse
+from django.views       import View
+from restaurants.models import Image, Restaurant
 
-# Create your views here.
+class RestaurantFoodImageView(View):
+    def get(self, request, restaurant_id):
+        try:
+            foods_queryset = Restaurant.objects.get(id=restaurant_id).foods.all()
+            images         = []
+
+            for food_instance in foods_queryset:
+                images_queryset = Image.objects.filter(food=food_instance)
+                
+                for image_instance in images_queryset:
+                    images.append(image_instance.image_url)
+            
+            return JsonResponse({"message":"success", "result":images}, status=200)
+
+        except Restaurant.DoesNotExist:
+            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)
