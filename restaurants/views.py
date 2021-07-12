6f9b962b454776a6da714c934e22dbf5cba11c76
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 22:27:44 +0900

    MODIFY : 이전 PR 리뷰 토대로 수정

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,20 +1,21 @@
 from django.http        import JsonResponse
 from django.views       import View
+
 from restaurants.models import Image, Restaurant
 
 class RestaurantFoodImageView(View):
     def get(self, request, restaurant_id):
         try:
-            foods_queryset = Restaurant.objects.get(id=restaurant_id).foods.all()
-            images         = []
+            foods  = Restaurant.objects.get(id=restaurant_id).foods.all()
+            images = []
 
-            for food_instance in foods_queryset:
-                images_queryset = Image.objects.filter(food=food_instance)
+            for f in foods:
+                images = Image.objects.filter(food=f)
                 
-                for image_instance in images_queryset:
-                    images.append(image_instance.image_url)
+                for i in images:
+                    images.append(i.image_url)
             
             return JsonResponse({"message":"success", "result":images}, status=200)
 
         except Restaurant.DoesNotExist:
-            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)
+            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=404)
